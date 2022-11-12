fef49582615e3a4b58b6db6dc90aef871b25ff0b
ricksonthompson <rickson.silva@grupoicts.com.br>
2022-11-12 19:58:48 -0400

    feat: auth interceptor

---
+++ src/modules/auth.module.ts
@@ -0,0 +1,29 @@
+import { Module } from "@nestjs/common"
+import { APP_GUARD } from "@nestjs/core"
+// import { JwtModule } from "@nestjs/jwt"
+import { RoleGuard } from "../configs/authentication/auth.guard"
+import { AuthService } from "../services/auth.service"
+import { AuthController } from "../controllers/auth.controller"
+import { RequestContextModule } from "nestjs-request-context"
+
+@Module({
+    imports: [
+        // JwtModule.register({
+        //     secret: process.env.SECRET_KEY_ACCESS_TOKEN,
+        //     signOptions: {
+        //         expiresIn: process.env.EXPIRES_IN_ACCESS_TOKEN
+        //     }
+        // }),
+        RequestContextModule
+    ],
+    controllers: [AuthController],
+    providers: [
+        AuthService,
+        { 
+            provide: APP_GUARD, 
+            useClass: RoleGuard
+        }
+    ]
+})
+
+export class AuthModule { }
