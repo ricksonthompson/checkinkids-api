9df501874e2a8e1a5dc5594bdb2f40c6ff1b2248
ricksonthompson <rickson.silva@grupoicts.com.br>
2023-02-06 17:59:23 -0400

    feat: add validation to register point

--- src/dtos/childrensOnCult/updatePointsChildrensOnCult.dto.ts
+++ src/dtos/childrensOnCult/updatePointsChildrensOnCult.dto.ts
@@ -1,6 +1,19 @@
+import { IsBoolean, IsOptional } from 'class-validator';
+
 export class UpdatePointsChildrensOnCultDTO {
+  @IsBoolean()
+  @IsOptional()
   verse?: boolean;
+
+  @IsBoolean()
+  @IsOptional()
   meditation?: boolean;
+
+  @IsBoolean()
+  @IsOptional()
   attendance?: boolean;
+
+  @IsBoolean()
+  @IsOptional()
   isInvited?: boolean;
 }
